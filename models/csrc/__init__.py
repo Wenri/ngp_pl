983b8a989634e96d04bdd6594fdbef4f24517df1
Bingchen Gong <wenri@wenri-dt.localdomain>
2022-07-28 17:17:29 +0800

    better hint

--- models/csrc/__init__.py
+++ models/csrc/__init__.py
@@ -1,2 +1,6 @@
 import torch
-from vren._C import *
+from vren._C import ray_aabb_intersect, ray_sphere_intersect, morton3D, morton3D_invert, packbits, \
+    raymarching_train, raymarching_test, composite_train_fw, composite_train_bw, composite_test_fw
+
+__all__ = ['ray_aabb_intersect', 'ray_sphere_intersect', 'morton3D', 'morton3D_invert', 'packbits',
+           'raymarching_train', 'raymarching_test', 'composite_train_fw', 'composite_train_bw', 'composite_test_fw']
