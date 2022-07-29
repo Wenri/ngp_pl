e4295b34ce777109ab390d7ca456a1975441d7bb
Bingchen Gong <wenri@wenri-dt.localdomain>
2022-07-29 16:29:30 +0800

    loss param

--- losses.py
+++ losses.py
@@ -13,28 +13,27 @@ def shiftscale_inv_depthloss(disp_pred, disp_gt):
         loss: (N)
     """
     t_pred = torch.median(disp_pred)
-    s_pred = torch.mean(torch.abs(disp_pred-t_pred))
+    s_pred = torch.mean(torch.abs(disp_pred - t_pred))
     t_gt = torch.median(disp_gt)
-    s_gt = torch.mean(torch.abs(disp_gt-t_gt))
+    s_gt = torch.mean(torch.abs(disp_gt - t_gt))
 
-    disp_pred_n = (disp_pred-t_pred)/s_pred
-    disp_gt_n = (disp_gt-t_gt)/s_gt
-    loss = (disp_pred_n-disp_gt_n)**2
+    disp_pred_n = (disp_pred - t_pred) / s_pred
+    disp_gt_n = (disp_gt - t_gt) / s_gt
+    loss = (disp_pred_n - disp_gt_n) ** 2
     return loss
 
 
 class NeRFLoss(nn.Module):
-    def __init__(self):
+    def __init__(self, lambda_opa=1e-3):
         super().__init__()
-
-        self.lambda_opa = 1e-3
+        self.lambda_opa = lambda_opa
 
     def forward(self, results, target, **kwargs):
         d = {}
-        d['rgb'] = (results['rgb']-target['rgb'])**2
+        d['rgb'] = (results['rgb'] - target['rgb']) ** 2
 
-        o = results['opacity']+1e-10
+        o = results['opacity'] + 1e-10
         # encourage opacity to be either 0 or 1 to avoid floater
-        d['opacity'] = self.lambda_opa*(-o*torch.log(o))
+        d['opacity'] = self.lambda_opa * (-o * torch.log(o))
 
         return d
